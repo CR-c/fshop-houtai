1ec10abe3489f9727662ce9ba3054ae97a1266e1
CR-C <2524670341@qq.com>
2023-11-29 14:30:56 +0800

    1

---
+++ src/main/java/cc/cat/CcCatApplication.java
@@ -0,0 +1,13 @@
+package cc.cat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcCatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcCatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cc/cat/CcCatApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.cat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcCatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
