1db7ac20c0dee3c65b1946f505860d2189ebe3e9
CR-C <2524670341@qq.com>
2024-04-01 09:05:58 +0800

    docker

--- src/main/java/cc/cat/controller/common/IndexWebController.java
+++ src/main/java/cc/cat/controller/common/IndexWebController.java
@@ -1,13 +1,18 @@
 package cc.cat.controller.common;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+//@RestController
+@Controller
 public class IndexWebController {
-    @RequestMapping({"/","index.html"})
-    public String hello(){
-        return "forward:index.html";
+    @RequestMapping("/")
+    public String test3()
+    {
+        return "index";
     }
 
 }
